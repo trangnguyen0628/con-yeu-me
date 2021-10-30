f129cdd0af9719af6721362396d5ce7e0c5e4c5e
Krystal Nguyen <trangnguyen0628@gmail.com>
2021-10-30 16:41:16 +0100

    Added image and create last page. Make old last page to another flippable page

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const front = document.querySelector(".front")
 const back = document.querySelector(".back")
 const page1 = document.querySelector(".page1")
 const page1Back = document.querySelector(".page1-back")
+const page2 = document.querySelector(".page2")
+const page2Back = document.querySelector(".page2-back")
 const flame = document.querySelector(".flame")
 
 front.addEventListener("click", () => {
@@ -15,10 +17,17 @@ page1.addEventListener("click", () => {
   page1.classList.toggle("open");
 });
 
-document.querySelector(".page2").addEventListener("click", () => {
+page2.addEventListener("click", () => {
+  page2Back.classList.toggle("open");
+  page2.classList.toggle("open");
+});
+
+document.querySelector(".page3").addEventListener("click", () => {
   back.classList.toggle("open");
   front.classList.toggle("open");
   flame.classList.toggle("open");
   page1Back.classList.toggle("open");
   page1.classList.toggle("open");
+  page2Back.classList.toggle("open");
+  page2.classList.toggle("open");
 });
